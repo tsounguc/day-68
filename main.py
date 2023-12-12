fc27159f17c739d022ed552fd65270c38a4e87b1
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-12 17:16:53 -0500

    flask flash messages

--- main.py
+++ main.py
@@ -6,22 +6,33 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
+login_manager = LoginManager()
+
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy()
 db.init_app(app)
 
+login_manager.init_app(app)
+
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
- 
- 
+
+
 with app.app_context():
     db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    with app.app_context():
+        user = db.get_or_404(User, user_id)
+    return user
 
 
 @app.route('/')
@@ -29,7 +40,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         name = request.form['name']
@@ -42,30 +53,50 @@ def register():
             db.session.commit()
 
             user_data = db.session.execute(db.select(User).where(User.email == email)).scalar()
-        return render_template('secrets.html', user=user_data)
+
+            login_user(user_data)
+
+        return redirect(url_for('secrets'))
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        with app.app_context():
+            user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))
+
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", user=current_user)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory("static", "files/cheat_sheet.pdf")
-
 
 
 if __name__ == "__main__":
